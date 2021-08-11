ba4643b699c5b778eeb4898378b4cf4f174e7dcd
Mateus Fonseca <mateus.xfl@gmail.com>
2021-08-11 12:25:51 -0300

    initializing the path dictionary

--- trabalho.py
+++ trabalho.py
@@ -8,7 +8,10 @@ totalDeArestas = int(primeiraLinha[1])
 listaDeRelacionamentos = list() # Declaramos um dicionário com a finalidade de armazenar os relacionamentos entre os vértices e também seus respectivos pesos.
 relacionamentos = dict(listaDeRelacionamentos)
 
-for linha in entrada: # Preenche o vetor de relacionamentos.
+listaDeCaminhos = list() # Declaramos um dicionário com a finalidade de armazenar o menor caminho de um vértice até outro.
+caminhos = dict(listaDeCaminhos)
+
+for linha in entrada: # Preenche o vetor de relacionamentos e inicializa o vetor de caminhos.
     primeiroVertice = int(linha.split()[0])
     segundoVertice = int(linha.split()[1])
     pesoAresta = int(linha.split()[2])
@@ -17,9 +20,15 @@ for linha in entrada: # Preenche o vetor de relacionamentos.
     if primeiroVertice not in relacionamentos: # Cria uma chave que não existe ainda no vetor de relacionamentos.
         relacionamentos[primeiroVertice] = list()
 
+    if primeiroVertice not in caminhos: # Inicializa o vetor de caminhos, que informa o menor caminho até um vértice.
+        caminhos[primeiroVertice] = -1   
+    elif segundoVertice not in caminhos:
+        caminhos[segundoVertice] = -1        
+
     relacionamentos[primeiroVertice].append([segundoVertice, pesoAresta]) # Adiciona o relacionamento X Y e seu respectivo peso.
 
 # print('Relacionamentos:',relacionamentos)
+# print('Caminhos:',caminhos)
 
 entrada.close()
 
