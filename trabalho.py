6d67a5b8d728592d0103aaacfb28ee4da1b27d97
Mateus Fonseca <mateus.xfl@gmail.com>
2021-08-23 17:02:28 -0300

    printing internal vertices

--- trabalho.py
+++ trabalho.py
@@ -1,4 +1,4 @@
-entrada = open('entradas/entrada2.txt','r') # Lê uma entrada TXT que contém o total de vértices / arestas na linha 1, e nas demais: os vértices que formam cada aresta, e seu respectivo peso.
+entrada = open('entradas/entrada1.txt','r') # Lê uma entrada TXT que contém o total de vértices / arestas na linha 1, e nas demais: os vértices que formam cada aresta, e seu respectivo peso.
 
 primeiraLinha = entrada.readlines(1)[0].split(' ')
 
@@ -8,42 +8,68 @@ totalDeArestas = int(primeiraLinha[1])
 listaDeRelacionamentos = list() # Declaramos um dicionário com a finalidade de armazenar os relacionamentos entre os vértices e também seus respectivos pesos.
 relacionamentos = dict(listaDeRelacionamentos)
 
-listaDeCaminhos = list() # Declaramos um dicionário com a finalidade de armazenar o menor caminho de um vértice até outro.
-caminhos = dict(listaDeCaminhos)
+listaDeMenorCaminho = list() # Declaramos um dicionário com a finalidade de armazenar o menor caminho até um certo vértice.
+menorCaminho = dict(listaDeMenorCaminho)
 
-for linha in entrada: # Preenche o vetor de relacionamentos e inicializa o vetor de caminhos.
+listaDeVerticeMaisProximo = list() # Declaramos um dicionário com a finalidade de armazenar o vértice mais próximo a outro vértice.
+verticeMaisProximo = dict(listaDeVerticeMaisProximo)
+
+for linha in entrada: # Preenche o vetor de relacionamentos e inicializa o vetor de menorCaminho e verticeMaisProximo.
     primeiroVertice = int(linha.split()[0])
     segundoVertice = int(linha.split()[1])
     pesoAresta = int(linha.split()[2])
-    # print(primeiroVertice,segundoVertice,pesoAresta)
 
     if primeiroVertice not in relacionamentos: # Cria uma chave que não existe ainda no vetor de relacionamentos.
         relacionamentos[primeiroVertice] = list()
 
-    if primeiroVertice not in caminhos: # Preenche o vetor de caminhos com valores ainda desconhecidos, informando que o mesmo ainda não foi visitado.
-        caminhos[primeiroVertice] = -1   
-    if segundoVertice not in caminhos:
-        caminhos[segundoVertice] = -1     
-
     relacionamentos[primeiroVertice].append([segundoVertice, pesoAresta]) # Adiciona o relacionamento X Y e seu respectivo peso.
-
-print('Relacionamentos:',relacionamentos)
-print('Caminhos:',caminhos)
-
-for vertice in caminhos: # Percorre todos os vértices do grafo.
-    if vertice == 1: # Caso o vértice seja o primeiro.
-        caminhos[vertice] = 0
-
-    if vertice in relacionamentos: # Se o vértice tem algum relacionamento com outro vértice, que ainda não tenha sido verificado.
-        for verticeAdjacente in relacionamentos[vertice]: # Percorre todos os vertices adjacentes do vertice atual.
-            adjacente = verticeAdjacente[0] # Captura o vértice adjacente ao mesmo.
-            pesoAresta = verticeAdjacente[1] # Peso da aresta dos dois vértices.
-
-            if caminhos[adjacente] == -1 or pesoAresta < caminhos[adjacente]: # Caso o caminho ainda não tenha sido visitado ou tenha sido encontrado um caminho menor.
-                caminhos[adjacente] = caminhos[vertice] + pesoAresta # O novo caminho até esse vértice é o caminho até o verticeAdjacente + peso da aresta.
-
-        # print('Caminhos', vertice, ':', caminhos) # Imprimindo cada execução para via de teste.
-
-print('Menores caminhos:',caminhos)
+    
+    if primeiroVertice not in menorCaminho: # Preenche o vetor de menorCaminho e verticeMaisProximo com valores ainda desconhecidos, informando que o mesmo ainda não foi visitado.
+        menorCaminho[primeiroVertice] = -1  
+        verticeMaisProximo[primeiroVertice] = -1   
+    if segundoVertice not in menorCaminho:
+        menorCaminho[segundoVertice] = -1
+        verticeMaisProximo[segundoVertice] = -1   
 
 entrada.close()
+
+# print('RELACIONAMENTOS:',relacionamentos)
+# print('MENOR CAMINHO:',menorCaminho)
+# print('VÉRTICE MAIS PRÓXIMO:',verticeMaisProximo)
+
+for vertice in menorCaminho: # Percorre todos os vértices do grafo.
+    if vertice == 1: # Caso o vértice seja o primeiro, a distância até ele mesmo é 0.
+        menorCaminho[vertice] = 0
+
+    if vertice in relacionamentos: # Se o vértice tem algum relacionamento com outro vértice.
+        for verticeAdjacente in relacionamentos[vertice]: # Percorre todos os vértices adjacentes ao vértice atual.
+            adjacente = verticeAdjacente[0] # Captura o vértice adjacente ao ao vértice atual.
+            pesoAresta = verticeAdjacente[1] # Peso da aresta formada pelos dois vértices.
+
+            if menorCaminho[adjacente] == -1 or pesoAresta < menorCaminho[adjacente]: # Caso o caminho ainda não tenha sido visitado ou tenha sido encontrado um caminho menor.
+                menorCaminho[adjacente] = menorCaminho[vertice] + pesoAresta # O novo caminho de adjacente é o caminho até o vértice atual + pesoAresta.
+                verticeMaisProximo[adjacente] = vertice # Informamos qual o vértice mais próximo de adjacente.
+
+        # print('MENOR CAMINHO', vertice, '° EXECUÇÃO :', menorCaminho)
+        # print('VERTICE PRÓXIMO', vertice, '° EXECUÇÃO :', verticeMaisProximo, '\n')
+
+ultimoVertice = len(menorCaminho) # Maior valor de vértice, ou seja, o último.
+
+arquivo = open("saida.txt", "w") # Escreve a saída (resposta).
+
+linhas = list() # Inicializa o vetor de linhas, para no fim adicioná-lo ao arquivo TXT de saída.
+
+verticesInternosNoCaminho = list()
+
+while verticeMaisProximo[ultimoVertice] != 1: # Preenche verticesInternosNoCaminho, executando enquanto não chegar no vértice inicial, tendo em vista que partimos do vértice final.
+    verticesInternosNoCaminho.append(verticeMaisProximo[ultimoVertice])
+    ultimoVertice = verticeMaisProximo[ultimoVertice]
+
+verticesInternosNoCaminho = verticesInternosNoCaminho[::-1] # Inverte os valores do vetor, para imprimir como solicitado.
+
+linhas.append(str(len(verticesInternosNoCaminho)) + '\n') # Adiciona o total de números na primeira linha, assim como é solicitado o retorno.
+print(str(len(verticesInternosNoCaminho)))
+
+for i in range(len(verticesInternosNoCaminho)): # Adiciona os demais vértices internos do menor caminho nas demais linhas.
+    linhas.append(str(verticesInternosNoCaminho[i]) + '\n')
+    print(str(verticesInternosNoCaminho[i]))
